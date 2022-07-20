57f8be304c326356b974a4b45d8022c6f1db62dd
Joao Paulo <joaopaulomorais2@gmail.com>
2022-07-20 14:05:27 -0300

    Criação da tabela de usuarios no bd, e adcionando drive java msql no projeto

--- Task Manager/SQL DB.sql
+++ Task Manager/SQL DB.sql
@@ -2,3 +2,15 @@ create database taskmanager;
 show databases;
 
 use taskmanager;
+
+create table tuser(
+id int primary key auto_increment,
+tipo int not null,
+nome varchar(50) not null,
+cpf varchar(11) not null,
+senha varchar(8) not null
+);
+
+insert into tuser (tipo,nome, cpf, senha) values(1,"João Paulo Carolina de Morais","04276602114","Jopacamo");
+
+select * from tuser;
---
+++ Task Manager/src/br/com/taskManeger/dal/ModuloConexao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.taskManeger.dal;
+
+import java.sql.*;
+/**
+ *
+ * @author joaop
+ */
+public class ModuloConexao {
+    //método resposavel por estabelecer a conexão com o banco de dados
+    public static Connection conector(){
+        java.sql.Connection conexao = null;
+        // a linha abaixo chama o driver 
+        String driver = "com.mysql.cj.jdbc.Driver";
+        
+        // parei aluna 10 14:45 minutos
+     return null;   
+    }
+}
