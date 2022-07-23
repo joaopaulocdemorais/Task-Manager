58ae94ed82ce05d2cba4e01a08b21ab1f3e7383a
Joao Paulo <joaopaulomorais2@gmail.com>
2022-07-23 15:12:41 -0300

    Inicio da criação do meto loga

--- Task Manager/src/br/com/taskManeger/telas/TelaLogin.java
+++ Task Manager/src/br/com/taskManeger/telas/TelaLogin.java
@@ -17,6 +17,18 @@ public class TelaLogin extends javax.swing.JInternalFrame {
     Connection conexao = null;
     PreparedStatement pst = null;
     ResultSet rs = null;
+    
+    public void logar(){
+        String sql = "select * from tuser where cpf=? and senha=?;";
+        try {
+            //as linhas abaixo prepara a consulta ao banco de dados
+            pst = conexao.prepareStatement(sql);
+            pst.setString(1, txtCpf.getText());
+            pst.setString(2, txtSenha.getText());
+            rs = pst.executeQuery();
+        } catch (Exception e) {
+        }
+    }
     /**
      * Creates new form TelaLogin
      */
@@ -25,7 +37,6 @@ public class TelaLogin extends javax.swing.JInternalFrame {
         getContentPane().setBackground(new Color(58,80,107));
         this.setVisible(true);
         conexao = ModuloConexao.conector();
-        System.out.println(conexao);
     }
 
     /**
@@ -40,10 +51,10 @@ public class TelaLogin extends javax.swing.JInternalFrame {
         jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
-        jTextField2 = new javax.swing.JTextField();
+        txtCpf = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
-        jPasswordField1 = new javax.swing.JPasswordField();
+        txtSenha = new javax.swing.JPasswordField();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
@@ -67,9 +78,9 @@ public class TelaLogin extends javax.swing.JInternalFrame {
             }
         });
 
-        jTextField2.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        jTextField2.setForeground(new java.awt.Color(58, 80, 107));
-        jTextField2.setSelectionColor(new java.awt.Color(51, 102, 255));
+        txtCpf.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        txtCpf.setForeground(new java.awt.Color(58, 80, 107));
+        txtCpf.setSelectionColor(new java.awt.Color(51, 102, 255));
 
         jLabel6.setBackground(new java.awt.Color(176, 226, 152));
         jLabel6.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
@@ -80,11 +91,11 @@ public class TelaLogin extends javax.swing.JInternalFrame {
         jLabel7.setForeground(new java.awt.Color(58, 80, 107));
         jLabel7.setText("Senha");
 
-        jPasswordField1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        jPasswordField1.setForeground(new java.awt.Color(58, 80, 107));
-        jPasswordField1.addActionListener(new java.awt.event.ActionListener() {
+        txtSenha.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        txtSenha.setForeground(new java.awt.Color(58, 80, 107));
+        txtSenha.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jPasswordField1ActionPerformed(evt);
+                txtSenhaActionPerformed(evt);
             }
         });
 
@@ -108,8 +119,8 @@ public class TelaLogin extends javax.swing.JInternalFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField2)
-                    .addComponent(jPasswordField1))
+                    .addComponent(txtCpf)
+                    .addComponent(txtSenha))
                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
@@ -118,11 +129,11 @@ public class TelaLogin extends javax.swing.JInternalFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel6)
                 .addGap(18, 18, 18)
-                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(txtCpf, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel7)
                 .addGap(18, 18, 18)
-                .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(txtSenha, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(64, 64, 64)
                 .addComponent(jButton1)
                 .addGap(29, 29, 29))
@@ -190,9 +201,9 @@ public class TelaLogin extends javax.swing.JInternalFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void jPasswordField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField1ActionPerformed
+    private void txtSenhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtSenhaActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jPasswordField1ActionPerformed
+    }//GEN-LAST:event_txtSenhaActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -205,7 +216,7 @@ public class TelaLogin extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField txtCpf;
+    private javax.swing.JPasswordField txtSenha;
     // End of variables declaration//GEN-END:variables
 }
